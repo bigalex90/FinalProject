376297a4b06b391a8debe2134530b2b6cd52a7f7
iskender onsoz <iskender_onsoz@hotmail.com>
2022-11-17 08:40:15 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System.Security.Cryptography.X509Certificates;
+using DataAccess.Concrete.EntityFramework;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(40,100))
+{
+    Console.WriteLine(product.ProductName);
+     
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden nesne olabilir
+    //new() ile IEntity newlenemez olduğu için Ientity yazılmasının önüne geçtim
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
